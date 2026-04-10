9c1e64ce2249214a87fa004c564e5c0f59dd5230
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-10 10:32:16 +0530

    learn the annotations and inference

---
+++ 02_annotationsAndInference/dist/index.js
@@ -0,0 +1,8 @@
+// inference(DATATYPE recognized by js itself)
+let name = "rudresh";
+console.log(name);
+// annotation (DATATYPE told by us)
+let indentifier = "rudresh";
+console.log(indentifier);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ 02_annotationsAndInference/src/index.ts
@@ -0,0 +1,8 @@
+// inference(DATATYPE recognized by js itself)
+let name = "rudresh";
+console.log(name);
+
+// annotation (DATATYPE told by us)
+let indentifier:string = "rudresh";
+
+console.log(indentifier);
